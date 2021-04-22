a63d9c85dbcddd1b1de7227558ed9a0b10dffc73
Brian Ross <brian.ross@students.makeschool.com>
2021-04-22 03:04:19 -0400

    users can see individual posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -24,4 +24,15 @@ module.exports = (app) => {
       })
   })
 
+  app.get("/posts/:id", function(req, res) {
+    // LOOK UP THE POST
+    Post.findById(req.params.id).lean()
+      .then(post => {
+        res.render("posts-show", { post });
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  });
+
 };
